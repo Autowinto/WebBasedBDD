ea3904b418c36bff5886912bc08737e7ed4ef1f4
Magnus Christian Larsen <happymansi@hotmail.com>
2024-10-29 18:57:17 +0100

    feat: added disabling button

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/scenarioUtils.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/scenarioUtils.js
@@ -1,3 +1,15 @@
-function checkInconsistencies() {
+function checkScenarios() {
 	alert("WORK IN PROGRESS")
+}
+
+function parseEntities(text) {
+	
+}
+
+function parseScenarios() {
+	
+}
+
+function enableButton(bool) {
+	document.getElementById("check-scenarios-btn").disabled = !bool
 }
